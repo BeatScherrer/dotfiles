2d1e439e52202303b0ee1409e1adcb02317f1a68
Beat Scherrer <beat.scherrer@gmail.com>
2021-08-03 22:51:13 +0200

    updated config

--- .config/bspwm/external_rules.sh
+++ .config/bspwm/external_rules.sh
@@ -12,8 +12,6 @@ case $class in
   '')
      pid=$(xprop -id $wid _NET_WM_PID | awk '{print $NF}')
      [ -n "$pid" ] && cmd="$(ps -q "$pid" -o comm= 2>/dev/null)"
-     [ "$cmd" = qemu-system-x86 ] && echo "state=floating desktop=^6"
-     [ "$cmd" = qemu-system-i386 ] && echo "state=floating desktop=^6"
      [ "$cmd" = spotify ] && echo "desktop=^5"
      ;;
 esac
