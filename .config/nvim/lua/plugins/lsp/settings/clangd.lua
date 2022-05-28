9a2e1b3ff04489cfea0bc6909ffe78888df1f3f4
Beat Scherrer <b.scherrer@mt-robot.com>
2022-05-29 01:21:58 +0200

    call schroot clangd

--- .config/nvim/lua/plugins/lsp/settings/clangd.lua
+++ .config/nvim/lua/plugins/lsp/settings/clangd.lua
@@ -5,12 +5,18 @@ local root_files = {
   'compile_commands.json'
 }
 
-
 return {
   root_dir = function(fname)
     return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
   end,
-  cmd = {"clangd"},
+  cmd = {
+    "/usr/bin/schroot",
+    "-c",
+    "chroot:mt_ubuntu18",
+    "--",
+    "clangd",
+    "--path-mappings=/usr=/srv/chroot/mt_ubuntu18/usr,/opt=/srv/chroot/mt_ubuntu18/opt"
+  },
   settings = {
   },
 }
