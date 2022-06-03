72c864f3ca2801452187d9f76124e56b43ccf234
Beat Scherrer <b.scherrer@mt-robot.com>
2022-06-04 00:35:14 +0200

    get rid of md plugin

--- .config/nvim/lua/packer_init.lua
+++ .config/nvim/lua/packer_init.lua
@@ -189,12 +189,6 @@ return packer.startup(function(use)
 		end,
 	})
 	use("karb94/neoscroll.nvim")
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	})
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
