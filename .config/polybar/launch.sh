94d094febeaa8223baeac6a715001357a27b3132
Beat Scherrer <beat.scherrer@gmail.com>
2022-12-15 14:25:29 +0100

    update polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,3 +11,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 polybar main -c ~/.config/polybar/config.ini &
 polybar secondary -c ~/.config/polybar/config.ini &
+polybar tertiary -c ~/.config/polybar/config.ini &
