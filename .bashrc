67f53cf093082047c29b48e79e1877a4de7698c8
Beat Scherrer <beat.scherrer@gmail.com>
2023-03-15 21:31:32 +0100

    update configs

--- .bashrc
+++ .bashrc
@@ -107,3 +107,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm use v16.15.1
+. "$HOME/.cargo/env"
--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -24,9 +24,51 @@ dap.adapters.coreclr = {
 	command = "/usr/sbin/netcoredbg",
 	args = { "--interpreter=vscode" },
 }
-
+dap.adapters.cppdbg = {
+	id = "cppdbg",
+	type = "executable",
+	command = "/home/beat/.vscode/extensions/ms-vscode.cpptools-1.14.4-linux-x64/debugAdapters/bin/OpenDebugAD7",
+}
 -- Configurations
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.cs = {
+	{
+		type = "coreclr",
+		name = "launch - netcoredbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to dll ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+		end,
+	},
+}
+
 dap.configurations.cpp = {
+	{
+		name = "Launch file",
+		type = "cppdbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopAtEntry = true,
+	},
+	{
+		name = "Attach to gdbserver :1234",
+		type = "cppdbg",
+		request = "launch",
+		MIMode = "gdb",
+		miDebuggerServerAddress = "localhost:1234",
+		miDebuggerPath = "/usr/bin/gdb",
+		cwd = "${workspaceFolder}",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+	},
+}
+
+-- dap.configurations.rust = dap.configurations.cpp
+dap.configurations.rust = {
 	{
 		name = "Launch",
 		type = "lldb",
@@ -35,7 +77,7 @@ dap.configurations.cpp = {
 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 		end,
 		cwd = "${workspaceFolder}",
-		stopOnEntry = false,
+		stopOnEntry = true,
 		args = {},
 		-- 💀
 		-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
@@ -49,17 +91,5 @@ dap.configurations.cpp = {
 		-- But you should be aware of the implications:
 		-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
 		-- runInTerminal = false,
-	},
-}
-dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
-dap.configurations.cs = {
-	{
-		type = "coreclr",
-		name = "launch - netcoredbg",
-		request = "launch",
-		program = function()
-			return vim.fn.input("Path to dll ", vim.fn.getcwd() .. "/bin/Debug/", "file")
-		end,
 	},
 }
