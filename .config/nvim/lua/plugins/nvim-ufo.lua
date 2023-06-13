2e569bcd937d38f1dc59637534485cf64ed85188
Beat Scherrer <beat.scherrer@gmail.com>
2023-06-13 21:45:38 +0200

    added folding with nvim-ufo

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -1,9 +1,42 @@
-vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
 return {
   "kevinhwang91/nvim-ufo",
-  requires = 'kevinhwang91/promise-async'
+  dependencies = { "kevinhwang91/promise-async" },
+  event = "BufRead",
+  keys = {
+    {
+      "zR",
+      function()
+        require("ufo").openAllFolds()
+      end,
+    },
+    {
+      "zM",
+      function()
+        require("ufo").closeAllFolds()
+      end,
+    },
+    {
+      "K",
+      function()
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end,
+    },
+  },
+  config = function()
+    vim.o.foldcolumn = "0"
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+    require("ufo").setup({
+      close_fold_kinds = { "imports" },
+    })
+  end,
 }
