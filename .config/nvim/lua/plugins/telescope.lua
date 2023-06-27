d10422ae5fc6e0fa35f022c0bd2701d734f8a80a
Beat Scherrer <beat.scherrer@gmail.com>
2023-06-27 21:03:46 +0200

    update nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
       build = "make",
       config = function()
         require("telescope").load_extension("fzf")
+        require("telescope").load_extension("just")
       end,
     },
     keys = {
