2e952285e628c052562d374717f1081cd72fafc9
Beat Scherrer <beat.scherrer@gmail.com>
2023-07-09 19:37:55 +0200

    update nvim comments for cpp

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -9,3 +9,11 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+-- Set the comment string of cpp to always use //
+vim.api.nvim_create_autocmd({ "BufEnter", "BufFilePost" }, {
+  pattern = { "*.c", "*.cpp", "*.h", "*.hpp" },
+  callback = function()
+    vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+  end,
+})
